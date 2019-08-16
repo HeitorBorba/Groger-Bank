94ce782efd0572fdc7d5c7a75433761d7b3e5978
heitor <heitorbc3@gmail.com>
2019-08-16 17:37:12 -0300

    Adicionar Groger-Bank

---
+++ README.md
@@ -0,0 +1,11 @@
+# Groger-Bank
+
+## **Participantes : **
+Heitor Borba
+Giovane Santesso
+
+## ** Dificuldades **
+- problema em fazer as funções 
+- organizar o codigo 
+- junção do codigo 
+- Não terminamos o emprestimo
---
+++ groger-bank.py
@@ -0,0 +1,46 @@
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+saldo = float(input('Digite seu saldo: '))
+
+informacoes = int(input('Digite 1 para sacar, \n'
+                    '            Digite (2) para fazer depósito, \n'
+                    '            Digite (3) para solicitar emprestimo, \n'
+                    '            Digite (4) para visualizar informaçoes, \n'
+                    '            Digite qualquer outro numero para sair: '))
+s
+
+def fsaque():
+    saque = float(input('Digite o quanto você deseja sacar: '))
+
+    if saque > 1000 or saq > saldo:
+        print('Seu saque deve ser maior que R$1000')
+
+    else:
+        print('Seu saldo agora é: ' + str(saldo - saque))
+
+
+def fdeposito():
+    deposito = float(input('quanto você quer depositar: '))
+
+    if deposito > 5000:
+        print('Valor errado')
+
+    else:
+        print('Seu saldo agora é: ' + str(saldo + deposito))
+
+def finformacoes():
+    print(nome)
+    print(idade)
+    print(saldo)
+
+if informacoes == 1:
+    fsaque()
+
+elif informacoes == 2:
+    fdeposito()
+
+elif informacoes == 4:
+    finformacoes()
+
+else:
+    print('Tchau, ate mais.')
